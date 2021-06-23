c15c51d42211da0fb1f9c42fee828ff91e01d8f3
planetskht <planetskht@users.noreply.github.com>
2021-06-23 19:14:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const
     res.header("Access-Control-Allow-Origin", '*');
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json,Authorization');
+    res.header("Access-Control-Allow-Headers", '*');
     next();
   });
 var fs = require("fs");
